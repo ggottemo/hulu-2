6a0a08c1f65fb8ad681748356eee83211101211e
ggottemo <ggottemo717@gmail.com>
2021-10-17 10:04:59 -0400

    navbar overflow fix

--- components/Nav.js
+++ components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     return (
         <nav className="relative">
            <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap
-            space-x-10 sm:space-x-20  overflow-x-scroll overflow-y-hidden scrollbar-hide '>
+            space-x-10 sm:space-x-20  overflow-x-scroll scrollbar-hide '>
             {Object.entries(requests).map(([key, {title, url }]) => (
                 <h2 key={key}
                 onClick={() => {
